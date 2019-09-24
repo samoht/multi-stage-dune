c0a94d01df9f0cc1de9b57b7293bb921261c39a2
Thomas Gazagnaire <thomas@gazagnaire.org>
2019-09-24 15:22:09 +0200

    have fun with dune action plugins

--- gen.ml
+++ gen.ml
@@ -1,10 +1,25 @@
 let () =
   let _ = Sys.command "mkdir -p bar" in
-  let bar_ml = open_out_bin "bar/bar.ml" in
-  output_string bar_ml "print_int Foo.x";
+  let bar_ml = open_out_bin "bar/dyn.ml" in
+  output_string bar_ml {|
+open Dune_action_plugin.V1
+
+let action =
+  let foo = read_file ~path:(Path.of_string "foo.ml") in
+  foo |> stage ~f:(fun _ ->
+    write_file
+     ~path:(Path.of_string "bar.ml")
+     ~data:("print_int Foo.x"))
+
+let () = run action
+|};
   close_out bar_ml;
   let dune = open_out_bin "bar/dune" in
-  output_string dune "(executable (name bar) (libraries foo))";
+  output_string dune {|
+(executable (name dyn) (libraries dune._dune_action_plugin))
+
+(alias (name bar) (action (dynamic-run ./bar.exe)))
+|};
   close_out dune;
-  let _ = Sys.command "dune exec -- bar/bar.exe" in
+  let _ = Sys.command "dune build @bar" in
   ()
